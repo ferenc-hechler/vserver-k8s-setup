132a3d25142bf9ecdfc8f5f88b619afca01bd20b
ferenc-hechler <github@fh.anderemails.de>
2023-01-02 23:57:06 +0100

    deploy from local copy.

--- setup-scripts/04-1-ingress-nginx.sh
+++ setup-scripts/04-1-ingress-nginx.sh
@@ -4,10 +4,13 @@
 #       https://github.com/kubernetes/ingress-nginx
 #       https://github.com/scriptcamp/nginx-ingress-controller
 #       https://medium.com/@ahmedwaleedmalik/exposing-tcp-and-udp-services-in-kubernetes-using-nginx-ingress-9b8fd639c576
+#       https://kubernetes.github.io/ingress-nginx/user-guide/nginx-configuration/configmap/#proxy-body-size
 
 set -xev
 cd $(dirname -- $0)
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
+# customized to allow file uploads > 1MB
 
-https://kubernetes.github.io/ingress-nginx/user-guide/nginx-configuration/configmap/#proxy-body-size
+kubectl apply -f 04-ingress-nginx/ingress-nginx-deploy.yaml
+
