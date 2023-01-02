f54ad57e199c47a908613fb39cc05b2955725f88
ferenc-hechler <github@fh.anderemails.de>
2023-01-02 16:51:47 +0100

    added run-all script for instant setup.

---
+++ setup-scripts/xx-run-all-scripts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xev
+cd $(dirname -- $0)
+
+./03-setup-k8s.sh
+./04-1-ingress-nginx.sh
+./04-2-route-default-ports.sh
+./05-cert-manager.sh
+./06-prometheus-grafana.sh
+./07-dashboard.sh
+./08-minio.sh
+./30-1-minecraft.sh
+./30-2-route-minecraft-port.sh
+# ./30-3-minecraft-backup.sh
+# ./30-4-minecraft-restore.sh
+./40-1-nexus.sh
+# ./40-2-nexus-backup.sh
+# ./40-3-nexus-restore.sh
