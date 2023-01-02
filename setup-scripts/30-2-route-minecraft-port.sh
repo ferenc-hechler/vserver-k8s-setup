63d19006201ede8f277938b490184e8b88da3752
ferenc-hechler <github@fh.anderemails.de>
2023-01-02 14:27:31 +0100

    change minecraft service to cluster-ip (without node port).

--- setup-scripts/30-2-route-minecraft-port.sh
+++ setup-scripts/30-2-route-minecraft-port.sh
@@ -3,8 +3,8 @@
 set -xev
 cd $(dirname -- $0)
 
-export MINECRAFT_NODEPORT=$(kubectl get service minecraft-service -n minecraft -o  go-template='{{ (index .spec.ports 0).nodePort}}')
+CLUSTER_IP=$(kubectl get service minecraft-service -n minecraft -o  go-template='{{ .spec.clusterIP }}')
+CLUSTER_PORT=$(kubectl get service minecraft-service -n minecraft -o  go-template='{{ (index .spec.ports 0).port}}')
 
-export PUBLIC_IP=$(curl ident.me)
+sudo nohup socat TCP-LISTEN:61267,fork TCP:$CLUSTER_IP:$CLUSTER_PORT  >/dev/null 2>&1 &
 
-sudo nohup socat TCP-LISTEN:61267,fork TCP:$PUBLIC_IP:$MINECRAFT_NODEPORT  >/dev/null 2>&1 &
