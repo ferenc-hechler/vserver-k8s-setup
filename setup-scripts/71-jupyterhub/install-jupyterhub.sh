908fffd5c1101b8f1285e603481b8cf5a8ce7990
Ferenc Hechler <ferenc.hechler@telekom.de>
2023-02-16 15:00:23 +0100

    add vs for jupyter.

--- setup-scripts/71-jupyterhub/install-jupyterhub.sh
+++ setup-scripts/71-jupyterhub/install-jupyterhub.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+# see https://github.com/jupyterhub/zero-to-jupyterhub-k8s/blob/2.0.0/images/hub/requirements.txt
+
 set -xev
 cd $(dirname -- $0)
 
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
-helm upgrade --install jupyter jupyterhub/jupyterhub --namespace spark --create-namespace --version=<chart-version> --values values.yaml
+helm upgrade --install jupyter jupyterhub/jupyterhub --namespace spark --create-namespace
 
 kubectl apply -f jupyter-vs.yaml
 
