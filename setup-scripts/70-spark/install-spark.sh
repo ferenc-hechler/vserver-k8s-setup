39ec226917f3809c1b34ec4691bf619824e469b0
Ferenc Hechler <ferenc.hechler@telekom.de>
2023-03-04 19:12:06 +0100

    increase number of workers to 4.

--- setup-scripts/70-spark/install-spark.sh
+++ setup-scripts/70-spark/install-spark.sh
@@ -6,6 +6,7 @@ cd $(dirname -- $0)
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm upgrade --install -n spark bit-spark --create-namespace bitnami/spark
+# helm upgrade --install -n spark bit-spark --values values.yaml --create-namespace bitnami/spark
 
 kubectl apply -f spark-vs.yaml
 
