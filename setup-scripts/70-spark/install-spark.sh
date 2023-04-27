74f94e6bf789375db124687341cd28cf58191695
Ferenc Hechler <ferenc.hechler@telekom.de>
2023-04-27 11:56:57 +0200

    remove unused files.

--- setup-scripts/70-spark/install-spark.sh
+++ setup-scripts/70-spark/install-spark.sh
@@ -5,7 +5,8 @@ cd $(dirname -- $0)
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm upgrade --install -n spark bit-spark --create-namespace bitnami/spark
+helm upgrade --install -n spark bit-spark --set "worker.replicaCount=2" --create-namespace bitnami/spark
+# helm upgrade --install bit-spark --version 6.1.10 --set "worker.replicaCount=2" -n spark --create-namespace bitnami/spark
 # helm upgrade --install -n spark bit-spark --values values.yaml --create-namespace bitnami/spark
 
 kubectl apply -f spark-vs.yaml
