2dd5d13966e32c743fc9871d5fcb198cc5d60f2a
Ferenc Hechler <ferenc.hechler@telekom.de>
2023-04-27 11:58:19 +0200

    remove zip file.

--- setup-scripts/61-nginx/js-ar/upload.cmd
+++ setup-scripts/61-nginx/js-ar/upload.cmd
@@ -5,3 +5,4 @@ tar cvzf ..\html.zip .
 cd ..
 kubectl cp -n nginx html.zip %POD_NAME%:usr/share/nginx/html/html.zip
 kubectl exec -it -n nginx %POD_NAME% -- tar -C /usr/share/nginx/html -xvzf /usr/share/nginx/html/html.zip
+del html.zip
