ab530e1b17709c884959a644ddc1bedf7c614311
ferenc-hechler <github@fh.anderemails.de>
2023-05-17 00:17:49 +0200

    added vault

--- setup-scripts/63-vault/install-vault.sh
+++ setup-scripts/63-vault/install-vault.sh
@@ -15,22 +15,30 @@ helm upgrade --install vault  -n vault --create-namespace hashicorp/vault --vers
 # https://github.com/hashicorp/vault-helm/issues/17
 #
 echo "kubectl exec -it -n vault vault-0 -- vault operator init"
-#  Unseal Key 1: UPdxbv8w3OLpdUX1L6XbyHhlWK/ghglGkDXTJfQdw8ED
-#  Unseal Key 2: bhavO5NcDCDcspgUvwG6MQ7F9+T1cRTpLB3zbYKRr4Vf
-#  Unseal Key 3: kv8O2iegM4PZAAJjaZfRuGd5vu1+N1f+Bjcx3urgz6xq
-#  Unseal Key 4: liOPhUTIXvQHGf2V9volHFWR4MI8c08tzHJkAWWP7MjQ
-#  Unseal Key 5: omuvUj6JJvCIPeSTSH7NPetjR9sXVc7pfNfrE9+36/1n
+#  Unseal Key 1: UPdx...dw8ED
+#  Unseal Key 2: bhav...r4Vf
+#  Unseal Key 3: kv8O...z6xq
+#  Unseal Key 4: liOP...7MjQ
+#  Unseal Key 5: omuv...6/1n
+#  Initial Root Token: hvs.GJOd...Ekjp
 
 echo "unseal with 3 of 5 keys"
   
 echo kubectl exec -it -n vault vault-0 -- vault operator unseal
-#  Unseal Key (will be hidden): UPdxbv8w3OLpdUX1L6XbyHhlWK/ghglGkDXTJfQdw8ED
+#  Unseal Key (will be hidden): UPdx...w8ED
 
 echo kubectl exec -it -n vault vault-0 -- vault operator unseal
-#  Unseal Key (will be hidden): bhavO5NcDCDcspgUvwG6MQ7F9+T1cRTpLB3zbYKRr4Vf
+#  Unseal Key (will be hidden): bhav...r4Vf
 
 echo kubectl exec -it -n vault vault-0 -- vault operator unseal
-#  Unseal Key (will be hidden): kv8O2iegM4PZAAJjaZfRuGd5vu1+N1f+Bjcx3urgz6xq
+#  Unseal Key (will be hidden): kv8O...z6xq
 
+kubectl apply -f vault-ing.yaml
  
+# https://developer.hashicorp.com/vault/docs/platform/k8s/helm/run
+
+# videos
+# https://developer.hashicorp.com/vault/tutorials/getting-started-ui/getting-started-intro-ui
+# https://developer.hashicorp.com/vault/tutorials/getting-started-ui/getting-started-policies-ui
+
  
