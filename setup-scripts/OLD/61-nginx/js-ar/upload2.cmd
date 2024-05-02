4818ef457e54a724a3fab999ad4afb5fbb63f58b
ferenc-hechler <github@fh.anderemails.de>
2024-05-02 23:02:38 +0200

    cleanup.

--- setup-scripts/OLD/61-nginx/js-ar/upload2.cmd
+++
@@ -1,8 +0,0 @@
-cd /D %~dp0
-set POD_NAME=nginx-c6b697b49-dmkwv
-cd html2
-tar cvzf ..\html.zip .
-cd ..
-kubectl cp -n nginx html.zip %POD_NAME%:usr/share/nginx/html/html.zip
-kubectl exec -it -n nginx %POD_NAME% -- tar -C /usr/share/nginx/html -xvzf /usr/share/nginx/html/html.zip
-del html.zip
--- setup-scripts/xx-run-all-scripts.sh
+++ setup-scripts/xx-run-all-scripts.sh
@@ -3,19 +3,11 @@
 set -xev
 cd $(dirname -- $0)
 
+export DOMAINLABEL1=cluster-1
+export HELMOPTS=--set=domainLabel1=$DOMAINLABEL1
+
 ./03-setup-k8s.sh
-./04-1-ingress-nginx.sh
-./04-2-route-default-ports.sh
-./05-cert-manager.sh
-./06-prometheus-grafana.sh
-./07-dashboard.sh
-./08-1-minio.sh
-# ./08-2-minio-backup.sh
-# ./08-3-minio-restore.sh
-./30-1-minecraft.sh
-./30-2-route-minecraft-port.sh
-# ./30-3-minecraft-backup.sh
-# ./30-4-minecraft-restore.sh
-./40-1-nexus.sh
-# ./40-2-nexus-backup.sh
-# ./40-3-nexus-restore.sh
+./10-metallb.sh
+./20-istio.sh
+./30-host-nginx-routes.sh
+./40-prometheus-grafana.sh
