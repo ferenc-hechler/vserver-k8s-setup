6d1f8547fe3662d5182882e05d7366a786811b73
ferenc-hechler <github@fh.anderemails.de>
2024-05-03 21:57:35 +0200

    add encrypt/decrpyt scripts.

---
+++ setup-scripts/00_helper/decrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo TARGET_FILE=$1
+test -n "${TARGET_FILE}"
+test -f "${TARGET_FILE}.gpg"
+
+PASSPHRASE=$(cat /etc/machine-id | md5sum |  head -c 30 | tail -c 28 | md5sum | head -c 32)
+
+rm -f "${TARGET_FILE}"
+
+gpg --batch --output "${TARGET_FILE}" --passphrase "$PASSPHRASE" --decrypt "${TARGET_FILE}.gpg"
---
+++ setup-scripts/00_helper/encrypt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo SOURCE_FILE=$1
+test -n $SOURCE_FILE
+test -f "${SOURCE_FILE}"
+
+
+PASSPHRASE=$(cat /etc/machine-id | md5sum |  head -c 30 | tail -c 28 | md5sum | head -c 32)
+
+rm -f "${SOURCE_FILE}.gpg"
+
+gpg --batch --output "${SOURCE_FILE}.gpg" --armor --passphrase "$PASSPHRASE" --symmetric "${SOURCE_FILE}"
