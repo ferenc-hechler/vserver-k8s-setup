1765f84074d2330c1d6cd798f0d5d9b8524cc412
feri <feri@DESKTOP-QSATAT0.speedport.ip>
2025-04-04 23:23:10 +0200

    quite

--- setup-scripts/04-2-route-default-ports.sh
+++ setup-scripts/04-2-route-default-ports.sh
@@ -6,7 +6,7 @@ cd $(dirname -- $0)
 export NGINX_HTTP_NODEPORT=$(kubectl get service ingress-nginx-controller -n ingress-nginx -o  go-template='{{ (index .spec.ports 0).nodePort}}')
 export NGINX_HTTPS_NODEPORT=$(kubectl get service ingress-nginx-controller -n ingress-nginx -o  go-template='{{ (index .spec.ports 1).nodePort}}')
 
-export PUBLIC_IP=$(curl ident.me)
+export PUBLIC_IP=$(curl -q ident.me)
 
 sudo nohup socat TCP-LISTEN:80,fork TCP:$PUBLIC_IP:$NGINX_HTTP_NODEPORT & >/dev/null 2>&1
 sudo nohup socat TCP-LISTEN:443,fork TCP:$PUBLIC_IP:$NGINX_HTTPS_NODEPORT &  >/dev/null 2>&1
